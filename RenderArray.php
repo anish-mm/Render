4dec194cd5f61e09d27dd6169ba00996dc1f5617
Anish M M <anishmathewdev@gmail.com>
2017-05-23 12:34:31 +0530

    Errors handled

--- RenderArray.php
+++ RenderArray.php
@@ -1,15 +1,25 @@
 <?php
-
 namespace Render;
 
+use Exception;
+
 class RenderArray {
+	 
+  public function preHandle(array $arr) {
+    try {
+      return $this->handle($arr);    
+    }  
+    catch (Exception $e) {
+      return 'Caught exception: ' .  $e->getMessage() . "\n";
+    }
+  }	 
 	  
   /*
   * calls appropriate renderer to render $arr
   */
-  public function handle(array $arr) {
+  private function handle(array $arr) {
     if (!isset($arr['type'])) {
-      return "Type not specified for Render Array";
+      throw new Exception("Type not specified for Render Array");
     }
     
     switch ($arr['type']) {
@@ -39,40 +49,49 @@ class RenderArray {
     /*
     * render array for table theme should contain
     * a 'type' = 'table',
-    * 'headers' : string array, contains the headers for the table
-    * 'rows' : array rows, contains arrays of strings/render arrays representing rows
+    * 'headers' : string array: contains the headers for the table
+    * 'rows' : array of rows: each row is an array of strings/render arrays representing rows
     */
    
     $headers = isset($table['headers']) ? $table['headers'] : NULL;
+    if (!isset($table['headers']) || !is_array($table['headers'])) {
+      throw new Exception("Table headers not set.");    
+    }    
+    else {
+      $rows = (isset($table['rows']) && is_array($table['rows'])) ? $table['rows'] : [];
     
-    $rows = isset($table['rows']) ? $table['rows'] : NULL;
+      //number of columns in the table
+      $col_count = count($headers);
+      
+      //output html for table 
+      $output = "<table>\n";
     
-    //number of columns in the table
-    $col_count = count($headers);
-    
-    //output html for table 
-    $output = "<table>\n";
-    
-    //assign the headers
-    $output .= "  <tr>\n";
-    foreach ($headers as $value) {
-      $output .= "    <th>" . $value . "</th>\n";
-    }
-    $output .= "  </tr>\n";
-    
-    //add html for the rows
-    if ($rows != NULL) {
+      //assign the headers
+      $output .= "  <tr>\n";
+      foreach ($headers as $value) {
+        $output .= "    <th>" . $value . "</th>\n";
+      }
+      $output .= "  </tr>\n";
+      
+      //add html for the rows
+      
       foreach ($rows as $row) {
+        if (!is_array($row)) {
+          throw new Exception("Row not proper in table Render Array");        
+        }	      	
         $output .= "  <tr>\n";
         foreach ($row as $key=> $val) {
           //if more data in row than col_count, discard trailing values
           if ($key < $col_count) {
-          	$output .= "    <td>"; 
-          	if (is_array($val) && isset($val['type']) && $val['type'] != 'page') {
+            $output .= "    <td>"; 
+            if (is_array($val) && isset($val['type']) && $val['type'] != 'page') {
           	  $output .= $this->handle($val);
           	}
-          	else {
+          	elseif (is_string($val)) {
           	  $output .= $val;
+          	}
+          	else {
+          	  throw new Exception("Row not proper in table Render Array");
           	}
           	$output .= "</td>\n";
           }
@@ -81,13 +100,13 @@ class RenderArray {
           }
         }
         $output .= "  </tr>\n";
-      }
-    }
+      }    
   
-    //conclude html for table
-    $output .= "</table>\n";
+      //conclude html for table
+      $output .= "</table>\n";
     
-    return $output;
+      return $output;
+    }
   }
   
   /*
@@ -98,24 +117,24 @@ class RenderArray {
      /*
     * render array for link will have
     * a 'type'= 'link',
-    * 'url'= url for the link,
-    * 'text' = link text
+    * 'url': string: url for the link,
+    * 'text': string: link text
     */
     
     extract($link, EXTR_SKIP);
+    
+    if (!isset($url)) {
+      throw new exception("Url not specified for link.");    
+    }
      
     //output html for link
     $output = "<a href=";
-    
-    if (!isset($url)) {
-      return("Url Not Set");
-    }
   
     //add the url  
     $output .= "\"$url\">\n";
     
     //add the link text
-    $output .= "  $text\n";
+    $output .= "  " . ((isset($text) && is_string($text)) ? $text : "Click Here") . "\n";
     
     //close the <a> tag
     $output .= "</a>\n";
@@ -127,9 +146,9 @@ class RenderArray {
     /*
     * render array for page will have
     * 'type' as 'page',
-    * 'title' = a string, heading of page,
-    * 'body'= a string or array of strings, render arrays for links, tables.
-    * 'tags' = array of strings.
+    * 'title': string: heading of page,
+    * 'body': string or array of strings/render arrays for links, tables.
+    * 'tags': array of link render arrays.
     */  
   
     extract($arr, EXTR_SKIP);  
@@ -151,11 +170,10 @@ class RenderArray {
           $output .= "<p>" . $val . "</p>\n";
         }
         elseif (is_array($val) && isset($val['type']) && $val['type'] !== 'page') {
-         $output .= $this->handle($val);  //error not checked...      
+         $output .= $this->handle($val);       
         } 
         else {
-          $output = "No renderer available for this array";
-          break;        
+        	 throw new Exception("No renderer available for this array / type");       
         }       
       }
     }
@@ -163,17 +181,23 @@ class RenderArray {
       $output .= "<p>" . $body . "</p>\n";
     }
     else {
-      $output = "No renderer available for this array";    
+      throw new Exception("No renderer available for this array / type");   
     }
     
     //add tags 
-    //for now added as ul of text.
     if (isset($tags) && is_array($tags)) {
       $output .= "<ul>\n";
       foreach ($tags as $val) {
-        $output .= "  <li>" . $val . "</li>\n";      
+        if (!is_array($val) || !isset($val['type']) || $val['type'] !== 'link') {
+          throw new Exception("No renderer available for this array / type");
+        }      	
+      	
+        $output .= "  <li>" . $this->handle($val) . "</li>\n";    
       }   
       $output .= "</ul>\n";
+    }
+    else {
+      throw new Exception("No renderer available for this array / type");
     }
     
     //conclude body and html tags
